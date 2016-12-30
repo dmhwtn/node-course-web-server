1150e22beb840bee7dc8a494538dadbe3459bc75
Dom <dchawton@gmail.com>
2016-12-30 22:48:10 +1100

    Added Projects page - third time lucky

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -39,13 +39,19 @@ hbs.registerHelper('screamIt', (text) => {
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home',
-    welcomeMessage: 'Hello, welcome to this empty website. Be safe.'
+    welcomeMessage: 'Hello, welcome to this empty website'
   })
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
